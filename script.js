f9b358f3826ff9170d9b424f2877ee6057c1c749
Gerard Bankhead <gerardb2827@hstat.org>
2021-03-08 16:47:14 +0000

    update repo

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/jvce-szsb.json',//The api for the mall
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+        //console.log(data);
+        var stores =[];
+        //Prints out all the stores
+        for(var i = 0; i<data.length; i++){
+           console.log(data[i].store)
+       }
     }
 });
