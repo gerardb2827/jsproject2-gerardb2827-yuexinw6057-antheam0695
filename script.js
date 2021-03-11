1157b078cf829a1da61b3baf493a67832478fdb4
Gerard Bankhead <gerardb2827@hstat.org>
2021-03-11 02:02:53 +0000

    answer type

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $.ajax({
     url: 'https://data.cityofnewyork.us/resource/jvce-szsb.json',//The api for the mall
     dataType: 'json',
     success: function(data) {
-        var answer = 1
+        var answer = 1;
         //prompt("What catagories would you like to look for?: ")
         //console.log(data);
         var stores =[];//Holds stores
@@ -11,22 +11,23 @@ $.ajax({
         //Prints out all the stores
         for(var i = 0; i<data.length; i++){
            //console.log(data[i].store)
-          var currentStore=data[i].store
+          var currentStore=data[i].store;
           if(!stores.includes(currentStore)){
-              stores.push(currentStore) //Holds all the stores in the mall in an array
+              stores.push(currentStore); //Holds all the stores in the mall in an array
           }//prints out the catagories
           
-          var currentType=data[i].type
+          var currentType=data[i].type;
           if(!types.includes(currentType)){
-              types.push(currentType) //Holds all the stores in the mall in an array
+              types.push(currentType); //Holds all the stores in the mall in an array
           }
-          //Number -> types
+          //user input used as a type of catagory for store
           var userChoice = (types[answer])
-          //List stores with that type
           
+          //List stores with that type
+         
           
        }//loop end
-      
+       document.write(userChoice)
        
         //console.log(stores + types)
     }
