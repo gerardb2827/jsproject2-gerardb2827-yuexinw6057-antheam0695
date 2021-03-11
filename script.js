0f7553b3d4503cb39e92955a641a3a28c273a933
Kayla Weng <yuexinw6057@hstat.org>
2021-03-11 02:01:51 +0000

    update repo

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ $.ajax({
         //prompt("What catagories would you like to look for?: ")
         //console.log(data);
         var stores =[];//Holds stores
-        var types =[];//Holds catagories 
+        var types = [];//Holds catagories 
         
         //Prints out all the stores
         for(var i = 0; i<data.length; i++){
            //console.log(data[i].store)
-          var currentStore=data[i].store
+          var currentStore = data[i].store
           if(!stores.includes(currentStore)){
               stores.push(currentStore) //Holds all the stores in the mall in an array
           }//prints out the catagories
           
-          var currentType=data[i].type
+          var currentType = data[i].type
           if(!types.includes(currentType)){
               types.push(currentType) //Holds all the stores in the mall in an array
           }
