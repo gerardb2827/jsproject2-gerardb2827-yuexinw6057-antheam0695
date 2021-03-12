b714875ecfff573fca85b62e9d3cae97600ad7ba
Kayla Weng <yuexinw6057@hstat.org>
2021-03-12 03:28:59 +0000

    update repo

---
+++ test1.js
@@ -0,0 +1,47 @@
+//Goal: Make user choose from number so that they can access it 
+
+$.ajax({
+    url: 'https://data.cityofnewyork.us/resource/jvce-szsb.json',//The api for the mall
+    dataType: 'json',
+    success: function(data) {
+        //pick # 0-39
+        var answer = prompt("What category are you looking for? Choose between numbers 0-39");
+        //console.log(data);
+        var stores = [];//Holds stores
+        var types = [];//Holds categories
+        
+        //Prints out all the stores
+        for(var i = 0; i < data.length; i++){
+
+          var currentStore = data[i].store
+          if(!stores.includes(currentStore)){
+              stores.push(currentStore); //Holds all the stores in the mall in an array
+          }//prints out the categories
+
+
+          var currentType = data[i].type
+          if(!types.includes(currentType)){
+              types.push(currentType); //Holds all the stores in the mall in an array
+          }
+          
+          if(data[i].type == userChoice){//Supposed to take the the choice and if the type matches it prints out the store
+//               var storeChoice = (data[i].store);
+              var store = curentStore
+          }
+
+       }//loop end
+       
+       //user input used as a type of category for store
+        var userChoice = types[answer]
+        var storeChoice = data.store;
+        
+        document.write("You have selected "+userChoice+", you should go to "+storeChoice)
+        
+       
+       }
+      
+        
+
+    });
+
+
