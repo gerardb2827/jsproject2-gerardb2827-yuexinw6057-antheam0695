b250c7999c4279700190ed2c67ac27db9e4d8996
Gerard Bankhead <gerardb2827@hstat.org>
2021-03-12 01:05:24 +0000

    update repo

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         //pick # 0-39
-        var answer = 39;
+        var answer = 29;
         //prompt("What catagories would you like to look for? Choose a number 0-39")
         //console.log(data);
         var stores = [];//Holds stores
         var types = [];//Holds catagories
-
+        
         //Prints out all the stores
         for(var i = 0; i < data.length; i++){
-           //console.log(data[i].store)
+           console.log(data[i].store)
 
           var currentStore = data[i].store
           if(!stores.includes(currentStore)){
@@ -22,30 +22,33 @@ $.ajax({
 
 
           var currentType = data[i].type
-
           if(!types.includes(currentType)){
               types.push(currentType); //Holds all the stores in the mall in an array
           }
+          
+        //   if(data[i].type== data[i].userChoice){
+        //       console.log(data.store)
+        //   }
 
        }//loop end
        
        //user input used as a type of catagory for store
+        
         var userChoice = (types[answer])
         console.log(userChoice)
+        
        
         
         //We want type to look for store with = type
         for(d = 0; d < data.length; d++){
-        if(data[d] == userChoice){
-            console.log(data.store)
-        }
+            if(data[d] == userChoice){
+                console.log(data.store)
+            }
         }
         //console.log(stores + types)
 
-          
           //console.log(types);
           //List stores with that type
-          
           
        }
       
